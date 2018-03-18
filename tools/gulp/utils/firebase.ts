a5609b1f7c37e562d6ed033a6f863f2071fac613
Oleg Pimenov <pimenov.o@gmail.com>
2018-03-18 23:39:55 +0300

    init commit

---
+++ tools/gulp/utils/firebase.ts
@@ -0,0 +1,22 @@
+const firebaseAdmin = require('firebase-admin');
+const firebase = require('firebase');
+
+const dashboardDatabaseUrl = 'https://mosaic-board.firebaseio.com/';
+
+export function openFirebaseDashboardApp() {
+
+    return firebaseAdmin.initializeApp({
+        databaseURL: dashboardDatabaseUrl,
+        credential: firebaseAdmin.credential.cert({
+            project_id: 'mosaic-board',
+            client_email: '',
+            private_key: decode(process.env['MOSAIC_BOARD_FIREBASE_SERVICE_KEY']!)
+        })
+    });
+}
+
+export function decode(str: string): string {
+    // In Travis CI the private key will be incorrect because the line-breaks are escaped.
+    // The line-breaks need to persist in the service account private key.
+    return (str || '').replace(/\\n/g, '\n');
+}
