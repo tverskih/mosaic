a1cd03d0588008771421534c74d3753551fb0234
Oleg Pimenov <pimenov.o@gmail.com>
2018-08-10 17:01:07 +0300

    chore: testing

--- .circleci/trigger_notify.sh
+++ .circleci/trigger_notify.sh
@@ -5,3 +5,21 @@ curl \
 -X POST \
 -H 'Content-Type: application/json' \
 -d '{"text": "😱 Hello, this is some text from CircleCI\nThis is more text. :tada:"}' https://chat.ptsecurity.com/hooks/rak3tz7fziybzr97buxw6xhzuy
+
+
+post_data=$(cat <<EOF
+{
+  "accountName": "Torkeldegaard",
+  "projectSlug": "logdisplayplatform",
+  "branch": "master"
+}
+EOF
+)
+
+echo ${post_data}
+
+curl \
+--header "Accept: application/json" \
+--header "Content-Type: application/json" \
+--data "${post_data}" \
+--request POST https://chat.ptsecurity.com/hooks/rak3tz7fziybzr97buxw6xhzuy
